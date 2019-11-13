dc123067b257d3e771da0e989de97a32dd3233ab
bourcierj <julesbourcier1@gmail.com>
2019-11-13 13:22:36 +0100

    Handles commant line args and hyperparams tracking in tensorboard

--- pos_tagging_gsd.py
+++ pos_tagging_gsd.py
@@ -1,5 +1,6 @@
 """Training a POS-tagger on the French GSD dataset."""
 from tqdm import tqdm
+import argparse
 
 import torch
 import torch.nn as nn
@@ -16,6 +17,8 @@ def train(checkpoint, criterion, train_loader, val_loader, epochs, patience=None
     net, optimizer = checkpoint.model, checkpoint.optimizer
     if patience is not None:
         early_stopper = EarlyStopper(patience)
+    if clip is None:  # no gradient clipping
+        clip = float('inf')
     writer = summary_writer
     min_loss = float('inf')
     iteration = 1
@@ -40,20 +43,24 @@ def train(checkpoint, criterion, train_loader, val_loader, epochs, patience=None
             epoch_loss += loss.item()
             pbar.set_postfix(loss=f'{loss.item():.4e}')
             if writer:
-                writer.add_scalar('Train/Iteration Loss', loss.item(), iteration)
+                writer.add_scalar('Iteration_loss', loss.item(), iteration)
             # compute gradients, update parameters
             loss.backward()
-            if clip is not None:
-                # gradient clipping helps prevent the exploding gradient problem in RNNs / LSTMs
-                torch.nn.utils.clip_grad_norm_(net.parameters(),
-                                               max_norm=clip)
+            # Gradient clipping helps prevent the exploding gradient problem in RNNs
+            # clip the gradients to the given clip value (+inf if not specified)
+            # and also return the total norm of parameters
+            total_norm = torch.nn.utils.clip_grad_norm_(net.parameters(),
+                                                        max_norm=clip)
+            if writer:
+                writer.add_scalar('Total_norm_of_parameters', total_norm, iteration)
+
             optimizer.step()
             iteration += 1
 
         epoch_loss /= len(train_loader)
-        print(f'Epoch {epoch}/{epochs}, mean loss: {epoch_loss:.4e}')
+        print(f'Epoch {epoch}/{epochs}, Mean loss: {epoch_loss:.4e}')
         if writer:
-            writer.add_scalar('Train/Epoch Loss', epoch_loss, epoch)
+            writer.add_scalar('Train_epoch_loss', epoch_loss, epoch)
         return epoch_loss
 
     def evaluate_epoch():
@@ -64,7 +71,7 @@ def train(checkpoint, criterion, train_loader, val_loader, epochs, patience=None
         with torch.no_grad():
             for data, lengths, target in val_loader:
                 data, lengths, target = data.to(device), lengths.to(device), \
-                                        target.to(device)
+                    target.to(device)
                 batch_size, seq_length = tuple(data.size())
                 output = net(data, lengths)
                 # flatten output and target to compute loss on individual words
@@ -81,11 +88,11 @@ def train(checkpoint, criterion, train_loader, val_loader, epochs, patience=None
 
         val_loss /= len(val_loader)
         acc = correct / total_preds
-        print(f'Epoch {epoch}/{epochs}, validation mean loss: {val_loss:.4e}, '
-              f'validation accuracy: {acc:.4f}')
+        print(f'Epoch {epoch}/{epochs}, Validation mean loss: {val_loss:.4e}, '
+              f'Validation accuracy: {acc:.4f}')
         if writer:
-            writer.add_scalar('Train/Val Loss', val_loss, epoch)
-            writer.add_scalar('Train/Val Accuracy', acc, epoch)
+            writer.add_scalar('Val_epoch_loss', val_loss, epoch)
+            writer.add_scalar('Val_epoch_accuracy', acc, epoch)
         return val_loss, acc
 
     losses = list()
@@ -107,7 +114,7 @@ def train(checkpoint, criterion, train_loader, val_loader, epochs, patience=None
                 break
 
     print("\nFinished.")
-    print(f"Best loss: {min_loss:.4e}")
+    print(f"Best validation loss: {min_loss:.4e}")
     if patience is not None:
         print(f"Best epoch: {early_stopper.min_epoch}")
     return losses
@@ -115,7 +122,24 @@ def train(checkpoint, criterion, train_loader, val_loader, epochs, patience=None
 
 if __name__ == '__main__':
 
+    def parse_args():
+        """Parse command line arguments."""
+        parser = argparse.ArgumentParser(description="Training a POS-tagger on the French"
+                                                     "GSD dataset.")
+        parser.add_argument('--batch-size', default=128, type=int)
+        parser.add_argument('--lr', default=0.001, type=float)
+        parser.add_argument('--epochs', default=30, type=int)
+        parser.add_argument('--patience', default=None, type=int)
+        parser.add_argument('--clip', default=None, type=float)
+        parser.add_argument('--embedding-size', default=50, type=int)
+        parser.add_argument('--hidden-size', default=50, type=int)
+        parser.add_argument('--num-layers', default=1, type=int)
+        parser.add_argument('--dropout', default=0, type=float)
+        parser.add_argument('--bidirectional', default=False, type=bool)
+        return parser.parse_args()
+
     torch.manual_seed(42)
+    args = parse_args()
 
     from torch.utils.data import DataLoader
     import torch.optim as optim
@@ -131,37 +155,46 @@ if __name__ == '__main__':
     words = VocabularyTagging(True)
     tags = VocabularyTagging(False)
     train_dataset = TaggingDataset(ds.files["train"], words, tags, True)
-    val_dataset = TaggingDataset(ds.files["dev"], words, tags, True)
+    val_dataset = TaggingDataset(ds.files["dev"], words, tags, False)
 
-    embedding_size = 50
-    hidden_size = 50
-    lr = 0.001
-    batch_size = 128
-    epochs = 10
-    patience = None
-    clip = None  # clips gradients norm
+    # manual settings
+    # lr = 0.001
+    # batch_size = 128
+    # epochs = 10
+    # patience = None
+    # clip = None  # clips gradients norm
+    # embedding_size = 50
+    # hidden_size = 50
+    # tensorboard_logdir = './runs/POS-Tagging-v0'
 
     kwargs = dict(num_workers=torch.multiprocessing.cpu_count(),
                   pin_memory=(device.type == 'cuda'))
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True,
                               collate_fn=TaggingDataset.collate, **kwargs)
 
-    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True,
+    val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=True,
                             collate_fn=TaggingDataset.collate, **kwargs)
 
-    net = Tagger(len(words), len(tags), embedding_size, hidden_size)
+    net = Tagger(len(words), len(tags), args.embedding_size, args.hidden_size,
+                 args.num_layers, args.dropout, args.bidirectional)
     net = net.to(device)
 
     # In order to exclude losses computed on null entries (zero),
     # set ignore_index=0 for the loss criterion
     criterion = nn.CrossEntropyLoss(ignore_index=0)
-    optimizer = optim.Adam(net.parameters(), lr=lr)
+    optimizer = optim.Adam(net.parameters(), lr=args.lr)
 
-    tensorboard_logdir = './runs/POS-Tagging-1'
-    writer = SummaryWriter(tensorboard_logdir)
+    # generate filename for the experiment
+    filename = '_'.join(
+        [f"{key.replace('_','-')}={value}" for key, value in vars(args).items()]
+    )
+    writer = SummaryWriter('./runs/POS-Tagging-GRU__'+filename)
+    # log sample data and net graph in tensorboard
+    data, lengths, target = next(iter(train_loader))
+    writer.add_graph(net, (data, lengths))
+
     savepath = './pos-tagger-checkpt.pt'
-
     checkpoint = CheckpointState(net, optimizer, savepath=savepath)
 
-    losses = train(checkpoint, criterion, train_loader, val_loader, epochs,
-                   patience=patience, clip=clip, summary_writer=writer)
+    losses = train(checkpoint, criterion, train_loader, val_loader, args.epochs,
+                   patience=args.patience, clip=args.clip, summary_writer=writer)
